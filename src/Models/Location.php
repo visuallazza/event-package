20b2bfc85ab381f6dcb8a9ca946464b221e350f2
Lasha Lobjanidze <lashalobjanidze@MacBook-Pro-von-Lasha.local>
2022-11-05 17:15:50 +0100

    Add Address.

--- src/Models/Location.php
+++ src/Models/Location.php
@@ -9,11 +9,5 @@ class Location extends DataTransferObject
     public string $name;
     public ?string $description;
     public ?string $link;
-    public ?string $street;
-    public ?string $houseNumber;
-    public ?string $addendum;
-    public ?string $city;
-    public ?string $zip;
-    public ?string $region;
-    public ?string $country;
+    public ?Address $address;
 }
